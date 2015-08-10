6a58779f313e6ca7f7d9da6752d552f14138af98
Andrew Darqui <andrew.darqui@gmail.com>
2015-08-10 04:19:40 -0400

    elems, indices, filter helpers

--- src/Daimyo/Data/Map.purs
+++ src/Daimyo/Data/Map.purs
@@ -1,8 +1,15 @@
-module Daimyo.Data.Map where
+module Daimyo.Data.Map (
+  toArray,
+  elems,
+  indices,
+  filterElems,
+  filterIndices,
+  filter
+) where
 
 import Prelude
-import Data.Array
 import Data.Tuple
+import qualified Data.List as L
 import qualified Data.Map as M
 
 import Daimyo.Data.ArrayList
@@ -12,3 +19,43 @@ import Daimyo.Data.ArrayList
 -- M.toList to an array
 toArray :: forall k v. M.Map k v -> Array (Tuple k v)
 toArray = listToArray <<< M.toList
+
+-- | elems
+--
+elems :: forall k v. M.Map k v -> L.List v
+elems = map snd <<< M.toList
+
+-- | indices
+--
+indices :: forall k v. M.Map k v -> L.List k
+indices = map fst <<< M.toList
+
+-- | filterElems
+--
+filterElems :: forall k v. (v -> Boolean) -> M.Map k v -> L.List (Tuple k v)
+filterElems f = filter (const f)
+
+-- | filterElems'
+--
+-- filterElems' :: forall k v. (v -> Boolean) -> M.Map k v -> L.List v
+-- filterElems' = map snd <<< filterElems
+
+-- | filterIndices
+--
+filterIndices :: forall k v. (k -> Boolean) -> M.Map k v -> L.List (Tuple k v)
+filterIndices f = filter (\k v -> f k)
+
+-- | filterIndices'
+--
+-- filterIndices' :: forall k v. (k -> Boolean) -> M.Map k v -> L.List v
+-- filterIndices' = map snd <<< filterIndices
+
+-- | filter
+--
+filter :: forall k v. (k -> v -> Boolean) -> M.Map k v -> L.List (Tuple k v)
+filter f = L.filter (uncurry f) <<< M.toList
+
+-- | filter'
+--
+-- filter' :: forall k v. (k -> v -> Boolean) -> M.Map k v -> L.List v
+-- filter' = map snd <<< filter
