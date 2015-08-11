b4272e9fd4a4c3da4d044d0f5561de3b8e5a6a22
Andrew Darqui <andrew.darqui@gmail.com>
2015-08-11 02:44:07 -0400

    some unoptimized array shuffling

---
+++ src/Daimyo/Data/Array/Shuffle.purs
@@ -0,0 +1,43 @@
+module Daimyo.Data.Array.Shuffle (
+  shuffleEff,
+  shuffleLCG_BSD,
+  shuffleLCG_MS
+) where
+
+import Prelude
+import Control.Monad.Eff
+import Control.Monad.Eff.Random
+import Control.Monad.State
+import Control.Monad.State.Class
+import Control.Monad.State.Trans
+import Data.Array
+import Data.Tuple
+
+import Daimyo.Control.Monad.Array
+import Daimyo.Random.LCG
+import Daimyo.Random.LCG.BSD
+import Daimyo.Random.LCG.MS
+
+import Daimyo.Data.ArrayList
+import qualified Daimyo.Data.List.Shuffle as S
+
+-- | shuffleEff
+--
+-- shuffle a list.
+-- a pure version of this would be great but i need this asap ;f
+--
+-- >>> shuffleEff (Cons 1 (Cons 2 (Cons 3 Nil)))
+-- Cons (2) (Cons (1) (Cons (3) (Nil)))
+--
+shuffleEff :: forall eff a. (Ord a) => Array a -> Eff (random :: RANDOM | eff) (Array a)
+shuffleEff xs = listToArray <$> S.shuffleEff (arrayToList xs)
+
+-- | shuffleLCG_BSD
+--
+shuffleLCG_BSD :: forall a. (Ord a) => Int -> Array a -> Array a
+shuffleLCG_BSD seed xs = listToArray $ S.shuffleLCG_BSD seed (arrayToList xs)
+
+-- | shuffleLCG_MS
+--
+shuffleLCG_MS :: forall a. (Ord a) => Int -> Array a -> Array a
+shuffleLCG_MS seed xs = listToArray $ S.shuffleLCG_MS seed (arrayToList xs)
