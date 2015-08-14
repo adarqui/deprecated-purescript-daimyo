fe3b431af9a2fe55fa4edcdd1d8e1d7724e59fed
Andrew Darqui <andrew.darqui@gmail.com>
2015-08-14 04:32:16 -0400

    intersperse, geet

--- src/Daimyo/Data/Array.purs
+++ src/Daimyo/Data/Array.purs
@@ -1,1 +1,23 @@
-module Daimyo.Data.Array where
+module Daimyo.Data.Array (
+  intersperse
+) where
+
+import Prelude
+import Data.Array
+import Data.Maybe
+
+-- | intersperse
+--
+-- >>> intersperse ',' "abcde" == "a,b,c,d,e"
+--
+intersperse :: forall a. a -> Array a -> Array a
+intersperse sep ys  = case uncons ys of
+                           Nothing                   -> []
+                           Just { head: h, tail: t } -> h : prependToAll sep t
+
+-- | prependToAll
+--
+prependToAll :: forall a. a -> Array a -> Array a
+prependToAll sep ys = case uncons ys of
+                           Nothing                   -> []
+                           Just { head: h, tail: t } -> sep : h : prependToAll sep t
