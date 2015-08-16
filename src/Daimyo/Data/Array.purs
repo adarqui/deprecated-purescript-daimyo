6856d8693ae5de844876f47bcf3c69594f5db1c7
Andrew Darqui <andrew.darqui@gmail.com>
2015-08-15 22:13:36 -0400

    lines

--- src/Daimyo/Data/Array.purs
+++ src/Daimyo/Data/Array.purs
@@ -1,10 +1,15 @@
 module Daimyo.Data.Array (
-  intersperse
+  intersperse,
+  break,
+  unlines,
+  lines
 ) where
 
 import Prelude
 import Data.Array
 import Data.Maybe
+import Data.String (joinWith, toCharArray, fromCharArray)
+import Data.Tuple
 
 -- | intersperse
 --
@@ -21,3 +26,32 @@ prependToAll :: forall a. a -> Array a -> Array a
 prependToAll sep ys = case uncons ys of
                            Nothing                   -> []
                            Just { head: h, tail: t } -> sep : h : prependToAll sep t
+
+-- | 'unlines' is an inverse operation to 'lines'.
+-- It joins lines, after appending a terminating neline to each.
+unlines :: Array String -> String
+unlines = joinWith "\n"
+
+-- | 'lines' breaks a string up into a list of strings at newline
+-- characters.  The resulting strings do not contain newlines.
+lines :: String -> Array String
+lines = reverse <<< map (fromCharArray <<< reverse) <<< go [] [] <<< toCharArray
+  where
+  go acc accs xs = case uncons xs of
+                        Nothing -> if accs == [] then acc else (accs : acc)
+                        Just {head:h, tail:t}
+                          | h == '\n' -> let f = if accs == [] then acc else (accs : acc) in go f [] t
+                          | otherwise -> go acc (h : accs) t
+
+-- | break
+--
+-- break (\c -> c == '\n') (toCharArray "hello\nworld!")
+--
+break :: forall a. (a -> Boolean) -> Array a -> Tuple (Array a) (Array a)
+break p [] = Tuple [] []
+break p xs = case uncons xs of
+                   Nothing -> Tuple [] []
+                   Just {head:x, tail:xs'}
+                    | p x       -> Tuple [] xs
+                    | otherwise -> let r = break p xs' in case r of
+                                       Tuple ys zs -> Tuple (x:ys) zs
